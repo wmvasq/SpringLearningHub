d0f79cfa9583bc4d161c64beaa4532989746c770
Marvin Vasquez <wmvasq1@gmail.com>
2023-06-19 23:08:33 -0400

    initial commit

---
+++ src/main/java/com/example/SpringLearningHub/SpringLearningHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringLearningHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningHubApplication.class, args);
+	}
+
+}
