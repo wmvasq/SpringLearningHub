87a7939e0fc0b1ba43c7358b6f447f9b88802350
Marvin Vasquez <wmvasq1@gmail.com>
2023-06-23 22:42:34 -0400

    decryptor added

--- src/main/java/com/SpringLearningHub/rest/Controller.java
+++ src/main/java/com/SpringLearningHub/rest/Controller.java
@@ -1,18 +1,37 @@
 package com.SpringLearningHub.rest;
 
 import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.SpringLearningHub.SpringLearningHub.SpringLearningHubApplication;
 
 @RestController
 public class Controller {
-    @GetMapping("/")
-  public String welcome() throws IOException {
 
-  /*  if (true) throw new ResponseStatusException(
+  @Autowired
+  SpringLearningHubApplication service;
+
+  @GetMapping("/")
+  public String welcome() throws IOException {
+    /*  if (true) throw new ResponseStatusException(
       HttpStatus.NOT_FOUND,
       "Not Found Exception."
     );*/
     return "Welcome to SpringLearningHub..";
+  }
+
+  @GetMapping("decryptor")
+  public ResponseEntity<String> decryptor(@RequestParam("token") String token)
+    throws IOException {
+    /*  if (true) throw new ResponseStatusException(
+      HttpStatus.NOT_FOUND,
+      "Not Found Exception."
+    );*/
+    return ResponseEntity.ok(service.decryptor(token));
   }
 }
